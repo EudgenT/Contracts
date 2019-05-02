51da1954c2dac4fc53747e266050432bc599cbac
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-05-02 07:45:30 +0300

    hotfix/ex-111/fixed naming

--- contracts_api/models.py
+++ contracts_api/models.py
@@ -5,15 +5,14 @@ metadata = MetaData()
 Contract = Table('Contract', metadata,
                  Column('id', String(32), primary_key=True),
                  Column('title', String(50), nullable=False),
-                 Column('contract_start_date', Date),
-                 Column('contract_end_date', Date),
-                 Column('customer', Integer, ForeignKey("contractor.contractor_id")),
-                 Column('executor', Integer, ForeignKey("contractor.contractor_id"))
+                 Column('start_date', Date),
+                 Column('end_date', Date),
+                 Column('customer', Integer, ForeignKey("Contractor.id")),
+                 Column('executor', Integer, ForeignKey("Contractor.id"))
                  )
 
-Contractor = Table('contractor', metadata,
-                   Column('contractor_id', Integer, primary_key=True),
+Contractor = Table('Contractor', metadata,
+                   Column('id', Integer, primary_key=True),
                    Column('title', String(50), nullable=False),
-                   Column('checking_account', Integer),
-                   Column('contract_end_date', Date)
+                   Column('checking_account', Integer)
                    )
