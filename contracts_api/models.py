22b169dcf609992d9e2e284eaaebbc480dd56418
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-05-03 11:54:06 +0300

    bugfix/ex-111/fixed all mentioned remarks to last commit

--- contracts_api/models.py
+++ contracts_api/models.py
@@ -2,17 +2,19 @@ from sqlalchemy import *
 
 metadata = MetaData()
 
-Contract = Table('Contract', metadata,
+Contract = Table(
+                 'Contract', metadata,
                  Column('id', String(32), primary_key=True),
                  Column('title', String(50), nullable=False),
                  Column('start_date', Date),
                  Column('end_date', Date),
                  Column('customer', Integer, ForeignKey("Contractor.id")),
                  Column('executor', Integer, ForeignKey("Contractor.id"))
-                 )
+            )
 
-Contractor = Table('Contractor', metadata,
-                   Column('id', Integer, primary_key=True),
+Contractor = Table(
+                   'Contractor', metadata,
+                   Column('id', String(32), primary_key=True),
                    Column('title', String(50), nullable=False),
                    Column('checking_account', Integer)
-                   )
+            )
