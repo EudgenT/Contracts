ad245ae2a5ab407d626da9ed3e660dc64f62a97b
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-05-07 12:37:19 +0300

    feature/ex-111/created sqlalchimy models

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Numeric, Date, MetaData, Table
+from sqlalchemy.dialects.postgresql import UUID
+
+metadata = MetaData()
+
+Contract = Table(
+                 'Contract', metadata,
+                 Column('id', UUID, primary_key=True),
+                 Column('title', String(50), nullable=False),
+                 Column('amount', Numeric),
+                 Column('start_date', Date),
+                 Column('end_date', Date),
+                 Column('customer', String(50), nullable=False),
+                 Column('executor', String(50), nullable=False),
+                )
+
+
