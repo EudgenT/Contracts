fa021931c7cd2007069f512b7c6fd6ab7029c80d
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-05-08 10:11:30 +0300

    bugfix/ex-113/removed @staticmethods

--- service_api/resourse/views.py
+++ service_api/resourse/views.py
@@ -6,43 +6,37 @@ from .forms import ContractSchema
 
 
 class SmokeView(HTTPMethodView):
-    @staticmethod
-    def get(request):
+    def get(self, request):
         return response.json({'message': 'Service enabled'}, headers={'Service': 'Contracts'}, status=200)
 
 
 class ContractsView(HTTPMethodView):
-    @staticmethod
-    async def get(request):
+    async def get(self, request):
         contracts = await get_all_contracts()
-        data = ContractSchema().dump(contracts, many=True)
-        return response.json(data)
+        result = ContractSchema().dump(contracts, many=True)
+        return response.json(result.data)
 
 
 class ContractView(HTTPMethodView):
-    @staticmethod
-    async def get(request, pk):
+    async def get(self, request, pk):
         contract = await get_contract(pk)
-        data = ContractSchema().dump(contract, many=True)
-        return response.json(data)
+        result = ContractSchema().dump(contract, many=True)
+        return response.json(result.data)
 
 
 class ContractCreateView(HTTPMethodView):
-    @staticmethod
-    async def post(request):
+    async def post(self, request):
         await create_contract(request.json)
         return text('Created')
 
 
 class ContractUpdateView(HTTPMethodView):
-    @staticmethod
-    async def put(request, pk):
+    async def put(self, request, pk):
         await update_contract(request.json, pk)
         return text('Updated')
 
 
 class ContractDeleteView(HTTPMethodView):
-    @staticmethod
-    async def delete(request, pk):
+    async def delete(self, request, pk):
         await delete_contract(pk)
         return text('Deleted')
