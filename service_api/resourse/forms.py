398f60b5125f620348962afe35185c64daa78927
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-05-10 18:29:05 +0300

    bugfix/ex-113/Serializing data is executed with default marshmallow validations. Inspected by flake8.

--- service_api/resourse/forms.py
+++ service_api/resourse/forms.py
@@ -1,21 +1,55 @@
 from marshmallow import Schema, fields, ValidationError, validates
 
 
-def must_not_be_blank(data):
-    if not data:
-        raise ValidationError('Data not provided.')
-
-
 class ContractSchema(Schema):
-    id = fields.UUID(required=True, validate=must_not_be_blank)
-    title = fields.String(validate=must_not_be_blank)
-    amount = fields.Decimal(validate=must_not_be_blank)
-    start_date = fields.Date(validate=must_not_be_blank)
-    end_date = fields.Date(validate=must_not_be_blank)
-    customer = fields.String(validate=must_not_be_blank)
-    executor = fields.String(validate=must_not_be_blank)
+    id = fields.UUID(required=True)
+    title = fields.String(required=True,
+                          error_messages={'required':
+                                          {'message': 'Title is required.',
+                                           'code': 400
+                                           }
+                                          }
+                          )
+    amount = fields.Decimal(required=True,
+                            error_messages={'required':
+                                            {'message': 'Amount is required.',
+                                             'code': 400
+                                             }
+                                            }
+                            )
+    start_date = fields.Date(required=True,
+                             error_messages={'required':
+                                             {'message': 'Start date is '
+                                                         'required.',
+                                              'code': 400
+                                              }
+                                             }
+                             )
+    end_date = fields.Date(required=True,
+                           error_messages={'required':
+                                           {'message': 'End is required.',
+                                            'code': 400
+                                            }
+                                           }
+                           )
+    customer = fields.String(required=True,
+                             error_messages={'required':
+                                             {'message': 'customer is '
+                                                         'required.',
+                                              'code': 400
+                                              }
+                                             }
+                             )
+    executor = fields.String(required=True,
+                             error_messages={'required':
+                                             {'message': 'executor is '
+                                                         'required.',
+                                              'code': 400
+                                              }
+                                             }
+                             )
 
     @validates('amount')
     def validate_amount(self, amount):
         if amount < 0:
-            raise ValidationError("Only positive numbers")
+            raise ValidationError("Amount can't be negative")
